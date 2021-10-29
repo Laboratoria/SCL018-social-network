f85e7f0038c7074a42bf3dbe3a16cfb5dc4f3e32
andreacarrascor <ancarami19@gmail.com>
2021-10-29 16:27:29 -0300

    instalacion de firebase en la terminal, importación con js desde firebase app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 // Este es el punto de entrada de tu aplicacion
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+// import { myFunction } from './lib/index.js';
+// myFunction();
 
-import { myFunction } from './lib/index.js';
+const firebaseConfig = {
+    apiKey: "AIzaSyAzfGqeX103yaOJ2nZuZuIu33UPNPPvwrA",
+    authDomain: "red-social-coders-scl018.firebaseapp.com",
+    projectId: "red-social-coders-scl018",
+    storageBucket: "red-social-coders-scl018.appspot.com",
+    messagingSenderId: "520088243760",
+    appId: "1:520088243760:web:5ac140eb0cdbe2b21e67f5",
+    measurementId: "G-9WBF6DXL6X"
+};
 
-myFunction();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
