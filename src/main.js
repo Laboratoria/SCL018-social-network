103de3d1ddbb5bc4d42729cc6664bc97deb8f2f4
Your Name <pau.emd@gmail.com>
2021-11-03 20:50:15 -0300

    intento de SPA con boton de crear cuenta

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable arrow-spacing */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable comma-spacing */
+/* eslint-disable space-in-parens */
+/* eslint-disable no-restricted-globals */
 /* eslint-disable eol-last */
 /* eslint-disable object-curly-newline */
 /* eslint-disable padded-blocks */
@@ -31,28 +37,46 @@ const auth = getAuth(app);
 console.log(app);
 const provider = new GoogleAuthProvider(app);
 
+// template sign-up
 
-// document.querySelector("#create-account").addEventListener("click", () => {
-//     const signUpEmail = document.getElementById("signup-email").value;
-//     const signUpPassword = document.getElementById("signup-password").value;
+const signUpTemplate = `<h1 class="coders-title">CODERS</h1>
+  <section class="logo-container">
+    <img
+      class="logo-coders"
+      src="./images/logo_principal.png"
+      alt="logo CODERS"
+    />
+  </section>
+  <input
+    type="email"
+    id="signup-email"
+    class="input-box"
+    name=""
+    placeholder="Ingrese su email"
+  />
+  <input
+    title="Debe tener 6 caracteres mínimo"
+    type="password"
+    id="signup-password"
+    class="input-box"
+    placeholder="Ingrese una contraseña"
+  />
+  <button class="login-button create-account">Crear cuenta</button>`;
+const rootHTML = document.getElementById("root");
 
-//     createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
-//         .then((userCredential) => {
-//             // Signed in
-//             const user = userCredential.user;
-//             // ...
-//             console.log("created");
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-//             // ..
-//             console.log(errorCode + errorMessage);
-//         });
-// })
+
+
+document.querySelector("#create-account-link").addEventListener("click", ()=>{
+    
+    rootHTML.innerHTML = ``;
+    rootHTML.innerHTML += signUpTemplate;
+    history.pushState(null, "","sign-up")
+
+
+})
 
 document.querySelector(".login-btn").addEventListener("click", () => {
-
+    
     const loginEmail = document.getElementById("login-email").value;
     const loginPassword = document.getElementById("login-password").value;
 
@@ -71,27 +95,47 @@ document.querySelector(".login-btn").addEventListener("click", () => {
 
 });
 
-document.querySelector(".login-google").addEventListener("click", () => {
-    signInWithRedirect(auth, provider);
-    getRedirectResult(auth)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access Google APIs.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
+// document.querySelector(".create-account").addEventListener("click", () => {
+//     history.pushState(null, "","login")
+//     const signUpEmail = document.getElementById("signup-email").value;
+//     const signUpPassword = document.getElementById("signup-password").value;
 
-            // The signed-in user info.
-            const user = result.user;
-            console.log("logged in with google");
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-            console.log(errorMessage);
-        });
+//     createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
+//         .then((userCredential) => {
+//             // Signed in
+//             const user = userCredential.user;
+//             // ...
+//             console.log("created");
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             // ..
+//             console.log(errorCode + errorMessage);
+//         });
+// })
 
-})
+// document.querySelector(".login-google").addEventListener("click", () => {
+//     signInWithRedirect(auth, provider);
+//     getRedirectResult(auth)
+//         .then((result) => {
+//             // This gives you a Google Access Token. You can use it to access Google APIs.
+//             const credential = GoogleAuthProvider.credentialFromResult(result);
+//             const token = credential.accessToken;
+
+//             // The signed-in user info.
+//             const user = result.user;
+//             console.log("logged in with google");
+//         }).catch((error) => {
+//             // Handle Errors here.
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             // The email of the user's account used.
+//             const email = error.email;
+//             // The AuthCredential type that was used.
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//             // ...
+//             console.log(errorMessage);
+//         });
+
+// })
