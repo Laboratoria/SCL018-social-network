a5e04198ef3c9a766f92e3b5116c77ab7f8dce5f
Your Name <pau.emd@gmail.com>
2021-11-08 18:30:31 -0300

    comienzo timeline

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-/* eslint-disable import/named */
 import { routes } from "./lib/index.js";
-=======
-// import { routes } from "./lib/index.js";
->>>>>>> 8e9b862150258d93755680407fc1addbad346bc6
 
 // window.addEventListener('load', () => {
 //   routes(window.location.hash);
