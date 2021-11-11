c7f68a14a61ddd73b3f64813eded9879e6bc97e0
andreacarrascor <ancarami19@gmail.com>
2021-11-11 12:22:42 -0300

    html dinamico pagina timeline

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
-// import { routes } from "./lib/index.js";
+import { routes } from "./lib/index.js";
 
-// window.addEventListener('load', () => {
-//   routes(window.location.hash);
-// });
-
-// window.addEventListener('hashchange', () => {
-//   routes(window.location.hash);
-// });
-
-//ESTO VA EN EL ARCHIVO DE JS DE TIMELINE
-const postHere = document.querySelector("#toggle-button");
-const createPost = document.querySelector("#create-post");
-const postWall = document.querySelector(".post-wall");
-const createPostContainer = document.querySelector(".create-post-container");
-postHere.addEventListener("click", () => {
-  postHere.classList.toggle("active");
-  createPost.classList.toggle("active");
-  postWall.classList.toggle("active");
-  createPostContainer.classList.toggle("active");
+window.addEventListener('load', () => {
+  routes(window.location.hash);
+});
+window.addEventListener('hashchange', () => {
+  routes(window.location.hash);
 });
