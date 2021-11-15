c4b7f8fa10b2793daa4a0afde5a6f83fd01e1010
Your Name <pau.emd@gmail.com>
2021-11-15 19:26:27 -0300

    intento de funciones asincronas

--- src/lib/timeline.js
+++ src/lib/timeline.js
@@ -1,16 +1,17 @@
 import { postear, leerData } from "../firebaseFile.js";
+import {showPost} from "../lib/showPost.js"; 
 
 
 
-export const showPost = (docData) => {
-  return `<section class="post-wall" id="post">
-  <div class="container-post">
-    <div class="header-post">${docData[0]}</div>
-    <div class="post-content"></div>
-    <div class="reference-link"></div>
-  </div>
-  </section>`;
-};
+// export const showPost = (docData) => {
+//   return `<section class="post-wall" id="post">
+//   <div class="container-post">
+//     <div class="header-post">${docData[0]}</div>
+//     <div class="post-content"></div>
+//     <div class="reference-link"></div>
+//   </div>
+//   </section>`;
+// };
 
 export const timeline = () => {
   const containerTimeline = document.createElement("section");
@@ -37,17 +38,28 @@ export const timeline = () => {
 
   containerTimeline.innerHTML = timelineHTML;
   const postButton = containerTimeline.querySelector(".post-button");
+  
+let docData;
   postButton.addEventListener("click", () => {
     const title = containerTimeline.querySelector("#textarea-title").value;
-    postear(title);
-    const docData = leerData();
-    docData.forEach(post => {
-      console.log(post);
-    });
-    
-    console.log(timelineHTML += showPost(docData));
-
+    async function printPost(){
+      await postear(title);
+      // const docData = leerData();
+      docData = await leerData();
+      let result = await timelineHTML.innerHTML + showPost(docData);
+      return result;
+    }
+    // console.log(leerData());
+    async function drawPost(){
+      await printPost();
+      timelineHTML += showPost(docData);
+      console.log(timelineHTML);
+    }
+    drawPost();
   });
+
+ 
+
 
   const postHere = containerTimeline.querySelector(".post-here-container");
   const postHereButton = containerTimeline.querySelector(".post-here-button");
