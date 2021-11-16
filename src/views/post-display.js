bb3dd391bb296b6fcefd05f4b10c9c65a8d3ba2d
Yésika Rodríguez <yesvrv@gmail.com>
2021-11-16 17:55:57 -0300

    se acomodo el post-user

--- src/views/post-display.js
+++ src/views/post-display.js
@@ -3,19 +3,21 @@ import { readData } from "../firebase.js";
 export const newPost = (posts) => {
   const newPostContainer = document.getElementById("postSection");
   newPostContainer.innerHTML = '';
-  const divPost = document.createElement("div");
-  divPost.className = "post-user";
+  // const divPost = document.createElement("div");
+  // divPost.className = "post-user";
 
   const feedContent = (e) => {
-    const feedHtml = `<div class="profile-container">
+    // console.log(e);
+    const feedHtml = `<div class="post-user"> 
+            <div class="profile-container">
             <div class="pic-container">
             <img class="pic-profile" src="resources/images/Vector-user.svg" alt="user" />
             </div>
             <h3 class="title-profile">Lady Gaga</h3>
             </div>
             <div class="textarea-container">
-            <textarea class="post-theme" id="postTheme" cols="10" rows="1">${e.theme}</textarea>
-            <textarea class="post-message" id="postMessage" cols="15" rows="5">${e.message}</textarea>
+            <textarea class="post-theme" id="postTheme" cols="10" rows="1" readonly>${e.theme}</textarea>
+            <textarea class="post-message" id="postMessage" cols="15" rows="5" readonly>${e.message}</textarea>
             </div>
             <div class="icons-container">
             <div class="like-container">
@@ -27,13 +29,14 @@ export const newPost = (posts) => {
             <div class="edit-container">
             <img src="./resources/images/edit.png" alt="edit">
             </div>
+            </div>
             </div>`;
 
     newPostContainer.innerHTML += feedHtml;
   };
 
   posts.forEach(feedContent);
-  newPostContainer.appendChild(divPost);
+  // newPostContainer.appendChild(divPost);
   return newPostContainer;
 };
 
