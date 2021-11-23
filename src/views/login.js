1fc7213d139afce5777c894c16f543652874edfe
lusana-avila <lusana.avila@gmail.com>
2021-11-23 08:22:12 -0300

    agregamos imagenes y estilo

--- src/views/login.js
+++ src/views/login.js
@@ -1,9 +1,29 @@
 export const loginUser = () => {
   const drawLogin = document.createElement('section');
   const loginTemplate = `
-<input type="email" placeholder="Email"></input>
-<input type="password" placeholder="Contraseña"></input>
-<button id="btnLogin">INICIAR SESIÓN</button>`;
+  
+  <section class="container-register">
+  <section class="container-images">
+  <img id="illustration" src="img/kambalache_ilustracion.png" alt="man and woman bartering things">
+  <img id="logo" src="img/kambalache_logo.png" alt="kambalache logo">
+  </section>
+    <section class="form">
+    <section class="email">
+      <input type="email" placeholder="Email"></input>
+    </section>
+    <section class="password">
+      <input type="password" placeholder="Contraseña"></input>
+    </section>
+    <section class="name">
+      <button>Iniciar Sesion</button>
+    </section>
+    <section class="register-here">
+      <h4>¿No tienes cuenta?<a href='#/register'>Registrate aqui</h4>
+    </section>
+  </section>
+</section>
+`;
+
 
   drawLogin.innerHTML = loginTemplate;
   return drawLogin;
