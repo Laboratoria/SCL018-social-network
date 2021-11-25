cbff13bfb9ae755b5b2f5287549338b82039b98a
Isabella X. Risso <ixrisso@gmail.com>
2021-11-25 10:09:48 -0300

    css en new post y feed

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,17 +5,17 @@ export const newPost = () => {
   const containerNewPost = document.createElement('section');
   containerNewPost.className = 'feed-container';
   containerNewPost.innerHTML = `
-        <section id='newpost' >
-        <input type='text' id='gameTitle' placeholder="Nombre del juego"/>
-        <textarea minlength='10' maxlength='1000' rows='10' columns='55' id='gameDescription' placeholder="Escribe aquí tu post"></textarea>
+        <section class='newpost' >
+        <input type='text' class='gameTitle' placeholder="Nombre del juego"/>
+        <textarea minlength='10' maxlength='1000' rows='10' columns='55' class='gameDescription' placeholder="Escribe aquí tu post"></textarea>
         <button id='publish'>PUBLICAR
         </button>
         </section>`;
 
   containerNewPost.querySelector('#publish').addEventListener('click', /* async */() => {
     /* aqui deben suceder cosas */
-    const gameTitle = containerNewPost.querySelector('#gameTitle').value;
-    const description = containerNewPost.querySelector('#gameDescription').value;
+    const gameTitle = containerNewPost.querySelector('.gameTitle').value;
+    const description = containerNewPost.querySelector('.gameDescription').value;
     posting(gameTitle, description);
     window.location.hash = '#/feed';
   });
